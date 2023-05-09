1072d871259a0f1485543fef965c828b5f96cbbd
Tuan <nguyentientuan2052000@gmail.com>
2023-05-09 08:32:11 +0700

    modify base on base code

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,15 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.1 <0.9.0;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+
+import "hardhat/console.sol";
+
 contract SimpleStorage is OwnableUpgradeable, EIP712Upgradeable {
     uint storedData;
+   address[] private whitelist = [
+        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+        0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+    ];
+    struct Set {
+        address sender;
+        uint256 x;
+        uint256 deadline;
+    }
+
+    bytes32 private constant _TYPEHASH = keccak256("Set(address sender,uint256 x,uint256 deadline)");
 
     function __SimpleStorage_init() internal initializer {
-        __Ownable_init();
-        __EIP712_init("SimpleStorage", "1");
+        __Ownable_init_unchained();
+        __EIP712_init_unchained("SimpleStorage", "1");
     }
 
     function set(uint x) internal {
@@ -19,27 +34,14 @@ contract SimpleStorage is OwnableUpgradeable, EIP712Upgradeable {
     function get() public view returns (uint) {
         return storedData;
     }
-
-    function executeSetIfSignatureMatch(
-        uint8 v,
-        bytes32 r,
-        bytes32 s,
-         address sender,
-        uint256 deadline,
-        uint x
-    ) external {
-       
-        require(block.timestamp < deadline, "Signed transaction expired");
-
-        bytes32 structHash = keccak256(
-            abi.encode(
-                keccak256("Set(address sender,uint256 x,uint256 deadline)"),
-                sender,
-                x,
-                deadline
-            )
-        );
-        bytes32 eip712DomainHash = keccak256(
+    
+    function getSigner(Set calldata req, bytes calldata signature)
+        public
+        view
+        returns (address)
+    {
+    bytes32 digest = keccak256(abi.encode(_TYPEHASH, req.sender, req.x, req.deadline));
+    bytes32 eip712DomainHash = keccak256(
         abi.encode(
             keccak256(
                 "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
@@ -51,12 +53,26 @@ contract SimpleStorage is OwnableUpgradeable, EIP712Upgradeable {
         )
     );  
 
-        bytes32 hash = keccak256(abi.encodePacked("\x19\x01", eip712DomainHash, structHash));
-        // bytes32 digest = _hashTypedDataV4(structHash);
+    bytes32 hash = keccak256(abi.encodePacked("\x19\x01", eip712DomainHash, digest));
+    address signer = ECDSAUpgradeable.recover(hash,signature);
 
-        address signer = ECDSAUpgradeable.recover(hash, v, r, s);
-        require(signer == sender, "SimpleStorage: invalid signature");
+    return signer;
+    }
+    function isSigner(address _signer) private view returns (bool) {
+        for (uint i = 0; i < whitelist.length; i++) {
+            if (whitelist[i] == _signer) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-        set(x);
+    function executeSetIfSignatureMatch(
+       Set calldata _req, bytes calldata signature
+    ) public {
+        address signer = getSigner(_req, signature);
+        require(isSigner(signer),"Invalid signature");
+        require(block.timestamp < _req.deadline, "Signed transaction expired");
+        set(_req.x);
     }
 }
--- test/simpleStorage.ts
+++ test/simpleStorage.ts
@@ -2,7 +2,6 @@ import { ethers } from "hardhat";
 import { expect } from "chai";
 import { Contract } from "ethers";
 import  { signTypedData_v4 } from "eth-sig-util";
-import { fromRpcSig } from "ethereumjs-util";
 
 describe("SimpleStorage", function () {
   let SimpleStorage;
@@ -10,17 +9,120 @@ describe("SimpleStorage", function () {
 
   let owner:any;
   let addr1:any;
+  let addr2: any;
 
   beforeEach(async function () {
     SimpleStorage = await ethers.getContractFactory("SimpleStorage");
     simpleStorage = await SimpleStorage.deploy();
 
-    [owner, addr1] = await ethers.getSigners();
+    [owner, addr1,addr2] = await ethers.getSigners();
     
   });
 
   describe("executeSetIfSignatureMatch", function () {
-    it("Should set the stored data to a new value with a valid signature", async function () {
+    it('Should set the stored data to a new value with a valid signature', async () => {
+        const value = 42;
+        const deadline = Math.floor(Date.now() / 1000) + 3600;
+
+        const message = {
+            sender: addr1.address,
+            x: value,
+            deadline: deadline,
+          };
+        const types = {
+            Set: [
+                { name: "sender", type: "address" },
+                { name: "x", type: "uint256" },
+                { name: "deadline", type: "uint256" },
+              ],
+        };
+        const domain =  {
+            name: "SimpleStorage",
+            version: "1",
+            chainId: (await ethers.provider.getNetwork()).chainId,
+            verifyingContract: simpleStorage.address,
+          }
+          
+        const signature = await owner._signTypedData(
+          domain,
+          types,
+          message
+        );
+    
+        await simpleStorage.executeSetIfSignatureMatch(
+          message,
+          signature
+        );
+    
+        expect(await simpleStorage.get()).to.equal(value);
+      });
+      it('Should revert with an expired signature', async () => {
+        const value = 42;
+        const deadline = Math.floor(Date.now() / 1000) - 3600;
+
+        const message = {
+            sender: addr1.address,
+            x: value,
+            deadline: deadline,
+          };
+        const types = {
+            Set: [
+                { name: "sender", type: "address" },
+                { name: "x", type: "uint256" },
+                { name: "deadline", type: "uint256" },
+              ],
+        };
+        const domain =  {
+            name: "SimpleStorage",
+            version: "1",
+            chainId: (await ethers.provider.getNetwork()).chainId,
+            verifyingContract: simpleStorage.address,
+          }
+          
+        const signature = await owner._signTypedData(
+          domain,
+          types,
+          message
+        );
+    
+        await expect(
+          simpleStorage.executeSetIfSignatureMatch(message,signature)
+        ).to.be.revertedWith("Signed transaction expired");
+      });
+      it('Should revert with invalid signature', async () => {
+        const value = 42;
+        const deadline = Math.floor(Date.now() / 1000) - 3600;
+
+        const message = {
+            sender: addr1.address,
+            x: value,
+            deadline: deadline,
+          };
+        const types = {
+            Set: [
+                { name: "sender", type: "address" },
+                { name: "x", type: "uint256" },
+                { name: "deadline", type: "uint256" },
+              ],
+        };
+        const domain =  {
+            name: "SimpleStorage",
+            version: "1",
+            chainId: (await ethers.provider.getNetwork()).chainId,
+            verifyingContract: simpleStorage.address,
+          }
+          
+        const signature = await addr2._signTypedData(
+          domain,
+          types,
+          message
+        );
+    
+        await expect(
+          simpleStorage.executeSetIfSignatureMatch(message,signature)
+        ).to.be.revertedWith("Invalid signature");
+      });
+    it("Should set the stored data to a new value with a valid signature with signTypedDataV4", async function () {
       // Define the parameters for the executeSetIfSignatureMatch function
       const sender = addr1.address; // The address of the signer
       const deadline = Math.floor(Date.now() / 1000) + 3600; // The expiration time of the signature
@@ -62,117 +164,14 @@ describe("SimpleStorage", function () {
         { data: typedData }
       );
   
-      // Parse the signature into v, r, and s components
-      const { v, r, s } = fromRpcSig(signature);
-  
       // Call the executeSetIfSignatureMatch function with the parameters and signature
-      await simpleStorage.executeSetIfSignatureMatch(v, r, s,sender, deadline, x);
+      await simpleStorage.executeSetIfSignatureMatch(typedData.message,signature);
   
       // Check that the stored data has changed
       expect(await simpleStorage.get()).to.equal(x);
     });
-
-    it("Should revert with invalid signature", async function () {
-      // Define the parameters for the executeSetIfSignatureMatch function
-      const sender = owner.address; // The address of the signer
-      const deadline = Math.floor(Date.now() / 1000) + 3600; // The expiration time of the signature
-      const x = 100; // The new value to set
-  
-      // Create a typed data object for EIP-712 signature
-      const typedData = {
-        types: {
-          EIP712Domain: [
-            { name: "name", type: "string" },
-            { name: "version", type: "string" },
-            { name: "chainId", type: "uint256" },
-            { name: "verifyingContract", type: "address" },
-          ],
-          Set: [
-            { name: "sender", type: "address" },
-            { name: "x", type: "uint256" },
-            { name: "deadline", type: "uint256" },
-          ],
-        },
-        primaryType: "Set",
-        domain: {
-          name: "SimpleStorage",
-          version: "1",
-          chainId: (await ethers.provider.getNetwork()).chainId,
-          verifyingContract: simpleStorage.address,
-        },
-        message: {
-          sender,
-          x,
-          deadline,
-        },
-      };
-  
-      // Sign the typed data with the sender's private key
-
-      const signature = signTypedData_v4(
-        Buffer.from("0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d".slice(2), "hex"),
-        { data: typedData }
-      );
-  
-      // Parse the signature into v, r, and s components
-      const { v, r, s } = fromRpcSig(signature);
-  
-      // Call the executeSetIfSignatureMatch function with the parameters and signature
-      await expect(
-        simpleStorage.executeSetIfSignatureMatch(v, r, s, sender, deadline, x)
-      ).to.be.revertedWith("SimpleStorage: invalid signature");
-    });
-    it("Should revert with an expired signature", async function () {
-      // Define the parameters for the executeSetIfSignatureMatch function
-      const sender = addr1.address; // The address of the signer
-      const deadline = Math.floor(Date.now() / 1000) - 3600; // The expiration time of the signature
-      const x = 100; // The new value to set
-  
-      // Create a typed data object for EIP-712 signature
-      const typedData = {
-        types: {
-          EIP712Domain: [
-            { name: "name", type: "string" },
-            { name: "version", type: "string" },
-            { name: "chainId", type: "uint256" },
-            { name: "verifyingContract", type: "address" },
-          ],
-          Set: [
-            { name: "sender", type: "address" },
-            { name: "x", type: "uint256" },
-            { name: "deadline", type: "uint256" },
-          ],
-        },
-        primaryType: "Set",
-        domain: {
-          name: "SimpleStorage",
-          version: "1",
-          chainId: (await ethers.provider.getNetwork()).chainId,
-          verifyingContract: simpleStorage.address,
-        },
-        message: {
-          sender,
-          x,
-          deadline,
-        },
-      };
-  
-      // Sign the typed data with the sender's private key
-
-      const signature = signTypedData_v4(
-        Buffer.from("0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d".slice(2), "hex"),
-        { data: typedData }
-      );
-  
-      // Parse the signature into v, r, and s components
-      const { v, r, s } = fromRpcSig(signature);
-  
-      // Call the executeSetIfSignatureMatch function with the parameters and signature
-      await expect(
-        simpleStorage.executeSetIfSignatureMatch(v, r, s, sender, deadline, x)
-      ).to.be.revertedWith("Signed transaction expired");
-  
-    });
+    
+    
 
   })
 })
